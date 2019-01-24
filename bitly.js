1dfa06e3a018922bfd5b1dbd41f00126692a64f0
jack.ding <jackding96@outlook.com>
2019-01-23 22:48:29 -0500

    trying to write to clipboard via chrome extension

--- bitly.js
+++
@@ -1,9 +0,0 @@
-var BitlyAPI = require("node-bitlyapi");
-var Bitly = new BitlyAPI({
-	client_id: "17cfeffb14a21238315c23501770003d1dd97a76",
-	client_secret: "c1ec91ecb596fe3dcf909e5ff8d725c070988b77"	
-});
-
-Bitly.authenticate(username, password, function(err, access_token) {
-	// Returns an error if there was one, or an access_token if there wasn't 
-});
