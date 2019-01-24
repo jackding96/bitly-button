6bff8308c6f0db31df163fa0f43c724d20524b28
jack.ding <jackding96@outlook.com>
2019-01-23 23:04:38 -0500

    more work trying to copy to clipboard

--- background.js
+++ background.js
@@ -11,24 +11,18 @@ function updateClipboard(newClip) {
 navigator.permissions.query({
   name: 'clipboard-write'
 }).then(permissionStatus => {
-  // Will be 'granted', 'denied' or 'prompt':
   console.log(permissionStatus.state);
-
-  // Listen for changes to the permission state
-  permissionStatus.onchange = () => {
-    console.log(permissionStatus.state);
-  };
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-  navigator.clipboard.writeText('Hello!')
-  .then(() => {
-    console.log('Text copied to clipboard');
-  })
-  .catch(err => {
-    console.error('Could not copy text: ', err);
-  });
+  // navigator.clipboard.writeText('Hello')
+  // .then(() => {
+  //   console.log('Text copied to clipboard');
+  // })
+  // .catch(err => {
+  //   console.error('Could not copy text: ', err);
+  // });
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     console.log(url);
