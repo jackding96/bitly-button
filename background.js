f2d8096bfd01a37beedb92ba0a1309c9748e0620
jack.ding <jackding96@outlook.com>
2019-01-24 10:38:09 -0500

    clean

--- background.js
+++ background.js
@@ -1,33 +1,9 @@
-function updateClipboard(newClip) {
-  navigator.clipboard.writeText('Hello!')
-  .then(() => {
-    console.log('Text copied to clipboard');
-  })
-  .catch(err => {
-    console.error('Could not copy text: ', err);
-  });
-} 
-
-navigator.permissions.query({
-  name: 'clipboard-write'
-}).then(permissionStatus => {
-  console.log(permissionStatus.state);
-});
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   browser.browserAction.openPopup()
 
-  // navigator.clipboard.writeText('Hello')
-  // .then(() => {
-  //   console.log('Text copied to clipboard');
-  // })
-  // .catch(err => {
-  //   console.error('Could not copy text: ', err);
-  // });
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     console.log(url);
 
-    
   });  
 });
