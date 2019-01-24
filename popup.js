fbf63523b192bd321ad7ed36c11177144611b9fa
jack.ding <jackding96@outlook.com>
2019-01-23 23:48:50 -0500

    undo green

--- popup.js
+++ popup.js
@@ -13,19 +13,4 @@ changeColor.onclick = function(element) {
       console.error('Could not copy text: ', err);
     });    
   });
-
-  // navigator.clipboard.writeText('Hello!')
-  // .then(() => {
-  //   console.log('Text copied to clipboard');
-  // })
-  // .catch(err => {
-  //   console.error('Could not copy text: ', err);
-  // });
-
-  let color = 'green';
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
 };
