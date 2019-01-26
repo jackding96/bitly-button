99a76ec24a4b431bd66cd147609238a783d877ab
jack.ding <jackding96@outlook.com>
2019-01-25 19:17:39 -0500

    prog on launching web auth flow from chrome extension

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  browser.browserAction.openPopup()
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   browser.browserAction.openPopup()
+// });
 
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    console.log(url);
-  });  
-  
-});
+var CLIENT_ID = '17cfeffb14a21238315c23501770003d1dd97a76';
+var CLIENT_SECRET = 'c1ec91ecb596fe3dcf909e5ff8d725c070988b77';
+var REDIRECT_URI = "https://gfkcjlimpeehdgkfcnkieoapmgnngolf.chromiumapp.org/testing"; 
+let ACCESS_TOKEN = '830df54fea45d0d3adc6e25b53fae0bb6c595a7b';
+
+console.log('Popup!');
+
+var loc = "https://bitly.com/oauth/authorize?client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI;
+chrome.identity.launchWebAuthFlow(
+  {'url': loc, 'interactive': true},
+  function(redirect_url) {
+    console.log('YEE');
+    console.log(redirect_url);
+   }
+);
--- popup.js
+++ popup.js
@@ -1,33 +1,47 @@
 let changeColor = document.getElementById('changeColor');
 
-let ACCESS_TOKEN = '830df54fea45d0d3adc6e25b53fae0bb6c595a7b';
+// var CLIENT_ID = '17cfeffb14a21238315c23501770003d1dd97a76';
+// var CLIENT_SECRET = 'c1ec91ecb596fe3dcf909e5ff8d725c070988b77';
+// var REDIRECT_URI = "https://gfkcjlimpeehdgkfcnkieoapmgnngolf.chromiumapp.org/testing"; 
+// let ACCESS_TOKEN = '830df54fea45d0d3adc6e25b53fae0bb6c595a7b';
 
-// changeColor.onclick = function(element) {
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    console.log(url);
+// console.log('Popup!');
 
-    fetch('https://api-ssl.bitly.com/v4/bitlinks', {
-      method: 'POST',
-      body: JSON.stringify({long_url: url}),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${ACCESS_TOKEN}`,
-      },
-    }).then(res => res.json())
-    .then(response => {
-      navigator.clipboard.writeText(response.link)
-      .then(() => {
-        document.getElementById('message-box').innerHTML = `
-          <p id='message-header'>Copied!</p>
-          <p id='message-link'>${response.link}</p>
-        `;
-        console.log('Text copied to clipboard');
-      })
-      .catch(err => {
-        console.error('Could not copy text: ', err);
-      }); 
-    })
-    .catch(error => console.error('NAW:', error));   
-  });
-// };
+// var loc = "https://bitly.com/oauth/authorize?client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI;
+// chrome.identity.launchWebAuthFlow(
+//   {'url': loc, 'interactive': true},
+//   function(redirect_url) {
+//     console.log('YEE');
+//     console.log(redirect_url);
+//    }
+// );
+
+// // changeColor.onclick = function(element) {
+//   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+//     var url = tabs[0].url;
+//     console.log(url);
+
+//     fetch('https://api-ssl.bitly.com/v4/bitlinks', {
+//       method: 'POST',
+//       body: JSON.stringify({long_url: url}),
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${ACCESS_TOKEN}`,
+//       },
+//     }).then(res => res.json())
+//     .then(response => {
+//       navigator.clipboard.writeText(response.link)
+//       .then(() => {
+//         document.getElementById('message-box').innerHTML = `
+//           <p id='message-header'>Copied!</p>
+//           <p id='message-link'>${response.link}</p>
+//         `;
+//         console.log('Text copied to clipboard');
+//       })
+//       .catch(err => {
+//         console.error('Could not copy text: ', err);
+//       }); 
+//     })
+//     .catch(error => console.error('NAW:', error));   
+//   });
+// // };
